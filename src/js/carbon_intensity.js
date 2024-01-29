f160b01f95bce259a698e1bb2f6898475bd3cb5e
Bala Devarajan <bkdev@amazon.com>
2024-01-28 17:49:36 -0800

    redo

--- src/js/carbon_intensity.js
+++ src/js/carbon_intensity.js
@@ -54,17 +54,17 @@ fetch('https://api.ipify.org?format=json')
             const result = await websiteCarbonCalculator.calculateByURL('https://shortcastovercoffee.com');
             console.log(result.co2PerPageview);
             if (result && result.co2PerPageview !== undefined) {
-                document.getElementById('data-container').textContent = `CO2 Emission per Pageview: ${result.co2PerPageview.toFixed(4)}g CO2`;
+                document.getElementById('wcb').textContent = `CO2 Emission per Pageview: ${result.co2PerPageview.toFixed(4)}g CO2`;
             } else {
-                document.getElementById('data-container').textContent = "Result object is missing or doesn't have the co2PerPageview property.";
+                document.getElementById('wcb').textContent = "Result object is missing or doesn't have the co2PerPageview property.";
             }
         } catch (error) {
             if (error instanceof WebsiteCarbonCalculatorError) {
                 console.warn('WebsiteCarbonCalculatorError:', error.message);
-                document.getElementById('data-container').textContent = 'WebsiteCarbonCalculatorError: ' + error.message;
+                document.getElementById('wcb').textContent = 'WebsiteCarbonCalculatorError: ' + error.message;
             } else {
                 console.error("An unexpected error occurred:", error);
-                document.getElementById('data-container').textContent = "An unexpected error occurred.";
+                document.getElementById('wcb').textContent = "An unexpected error occurred.";
             }
         }
     }
